fa4b81a82f1eea4d46361a4009e60e7605f85f62
ZoranWong <904110231@qq.com>
2018-11-19 23:51:20 +0800

    新建新零售平台

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,27 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('mobile', 11)
+                ->nullable()
+                ->comment('注册手机');
+            $table->string('email')
+                ->nullable()
+                ->comment('注册邮箱');
+            $table->timestamp('email_verified_at')
+                ->nullable()
+                ->comment('邮箱认证时间');
+            $table->string('nickname', 16)
+                ->nullable()
+                ->comment('昵称');
+            $table->json('region')
+                ->default([])
+                ->comment('区域');
+            $table->enum('sex', ['UNKNOWN', 'MALE', 'FEMALE'])
+                ->default('UNKNOWN')
+                ->comment('性别');
+            $table->json('roles')
+                ->default([])
+                ->comment('角色');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
